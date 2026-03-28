69c3d4581c78cc74adcc30739fe0a1fd20f22782
Krinxy <dominic.bechtold@icloud.com>
2026-03-28 16:07:01 +0100

    Refactor: Remove type annotation for Jest config and clean up tsconfig

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,4 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+const config = {
   roots: ['<rootDir>/services', '<rootDir>/packages', '<rootDir>/tests'],
   testEnvironment: 'node',
   transform: {
